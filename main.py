ed978734383b5370f800f5eb99679a27daa57664
PetoskeyCh022 <PetoskeyCh022@northwested.org>
2025-03-31 11:05:52 -0400

    Add files via upload

--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pygame
 import sys
 import config # Import the config module
 
+
 def init_game():
   pygame.init()
   screen = pygame.display.set_mode((config.WINDOW_WIDTH, config.WINDOW_HEIGHT)) # Use constants from config
   pygame.display.set_caption(config.TITLE)
+
   return screen
 
 def handle_events():
@@ -20,10 +22,29 @@ def handle_events():
 def main():
   screen = init_game()
   clock = pygame.time.Clock() # Initialize the clock here
+
+  bg_pos = [0, 0]
+
+  bg_img = pygame.image.load("saturn_family1.jpg").convert()
+  player_img = pygame.image.load("player.png").convert()
+  player_img.set_colorkey(config.BLACK)
+
+
   running = True
   while running:
+    
     running = handle_events()
-    screen.fill(config.WHITE) # Use color from config
+    # screen.fill(config.WHITE) 
+
+    screen.blit(bg_img, bg_pos)
+
+    player_pos = pygame.mouse.get_pos()
+    x = player_pos[0]
+    y = player_pos[1]
+
+    screen.blit(player_img, [x, y])
+
+  
     pygame.display.flip()
 
     # Limit the frame rate to the specified frames per second (FPS)
@@ -34,3 +55,4 @@ def main():
 
 if __name__ == "__main__":
   main()
+  
